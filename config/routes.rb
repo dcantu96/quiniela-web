e157500f497ab87e5d034b345f0875a62674956b
David Cantu Martinez <davidcantu966@gmail.com>
2020-09-07 00:53:57 -0500

    Added picks

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,7 @@ Rails.application.routes.draw do
   end
 
   resources :accounts
+  resources :memberships
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
