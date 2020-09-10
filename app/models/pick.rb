9b13f4778e496da83e3e90fe9cf61c25404a8faa
David Cantu Martinez <davidcantu966@gmail.com>
2020-09-10 18:52:16 -0500

    Added pick admins

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -4,14 +4,6 @@ class Pick < ApplicationRecord
   belongs_to :membership_week
   belongs_to :picked_team, class_name: 'Team', inverse_of: :picks, optional: true
   scope :filter_by_week_number, -> (group_week) { where group_week: group_week }
-  # validate :pick_not_passed_time
-
-
-  # def pick_not_passed_time
-  #   if ((Time.now > match.start_time-30.minutes || Time.now > group_week.week.start_time && !match.untie) || (match.untie && Time.now > match.start_time-30.minutes)) && !self.new_record?
-  #     errors.add :date, 'Match or week has started.'
-  #   end
-  # end
 
   def viewable?
     Time.current > match.show_time
