5b357142dfce381646e2d0f10155193bca1bd15a
David Cantu Martinez <davidcantu966@gmail.com>
2020-09-10 23:55:38 -0500

    Fix

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -4,7 +4,7 @@ class Pick < ApplicationRecord
   belongs_to :membership_week
   belongs_to :picked_team, class_name: 'Team', inverse_of: :picks, optional: true
   scope :filter_by_week_number, -> (group_week) { where group_week: group_week }
-  validate :check_if_correct
+  validate :check_if_correct, if: :picked_team_id_changed?
 
   def viewable?
     Time.current > match.show_time
@@ -17,8 +17,14 @@ class Pick < ApplicationRecord
   private
 
   def check_if_correct
-    if match.winning_team && picked_team == match.winning_team
-      self.correct = true
+    if match.winning_team
+      self.correct = picked_team == match.winning_team
+      new_points = match.premium ? 2 : 1
+      if correct
+        membership_week.update points: membership_week.points + new_points 
+      else
+        membership_week.update points: membership_week.points - new_points
+      end
     end
   end
 end
