a9b8b46f1e4c76617985cb66bba2afdabf8b59b4
David Cantu Martinez <davidcantu966@gmail.com>
2020-09-17 20:52:57 -0500

    Fixed search

--- app/javascript/controllers/searchable_controller.js
+++ app/javascript/controllers/searchable_controller.js
@@ -1,24 +1,18 @@
 import {Controller} from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['filter', 'rows']
+  static targets = ['filter', 'tablebody']
 
   search(event) {
-    var input, filter, tableRows, tr, td, i, txtValue;
-    input = event.target;
-    filter = input.value.toUpperCase();
-    tableRows = document.getElementById("table-rows");
-    tr = tableRows.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("th")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
+    let filter = event.target.value.toUpperCase();
+
+    for (let row of this.tablebodyTarget.rows) {
+      let txtValue = row.cells[0].textContent || row.cells[0].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
     }
   }
 }
