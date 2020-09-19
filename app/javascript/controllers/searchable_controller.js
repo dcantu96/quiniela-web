98c6f6edc57f6ea84ae367090cda5a761c0e7962
David Cantu Martinez <davidcantu966@gmail.com>
2020-09-19 14:14:26 -0500

    Fix

--- app/javascript/controllers/searchable_controller.js
+++ app/javascript/controllers/searchable_controller.js
@@ -1,17 +1,15 @@
 import {Controller} from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['filter', 'tablebody']
+  static targets = ['filter', 'member']
 
   search(event) {
-    let filter = event.target.value.toUpperCase();
-
-    for (let row of this.tablebodyTarget.rows) {
-      let txtValue = row.cells[0].textContent || row.cells[0].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        row.style.display = "";
+    let filter = event.target.value.toUpperCase()
+    for (let member of this.targets.findAll('member')) {
+      if (member.dataset.username.toUpperCase().indexOf(filter) > -1) {
+        member.style.display = "";
       } else {
-        row.style.display = "none";
+        member.style.display = "none";
       }
     }
   }
