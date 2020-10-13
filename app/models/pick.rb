e321accc11b490e8d12ce9636a9d6fefddfbdcae
David Cantu Martinez <davidcantu966@gmail.com>
2020-10-12 21:58:04 -0500

    Fix

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -6,7 +6,7 @@ class Pick < ApplicationRecord
   scope :filter_by_week_number, -> (group_week) { where group_week: group_week }
   scope :viewable, -> { joins(:match).where('matches.start_time < ?', Time.current) }
   scope :forgotten, -> { joins(:match).where("picks.picked_team_id IS NULL AND matches.winning_team_id IS NULL AND matches.tie = false AND (:time_limit > matches.start_time)", time_limit: Time.current + 6.hours) }
-  validate :check_if_correct, if: :picked_team_id_changed?
+  before_save :check_if_correct, if: :will_save_change_to_picked_team_id?
   before_save :update_points, if: :will_save_change_to_correct?
   validate :membership_week_must_be_same_as_match_week
 
