370e55d5641d614268e2d7e67704589c1f44c01c
David Cantu <davidcantu966@gmail.com>
2021-08-18 20:44:42 -0500

    update seed to working version

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 User.create email: 'example@admin.com',
             password: 123456,
-            phone: 8181818181,
+            phone: 8110228910,
             full_name: 'David Cantu'
 
-User.last.confirm
 User.last.add_role :admin
 
 Sport.create name: 'American Football'
 Tournament.create name: 'National Football League', sport: Sport.first
-Group.create name: 'QNFL MTY 2020', tournament: Tournament.first
+Group.create name: 'QNFL MTY 2021', tournament: Tournament.first
 
 TEAMS = [
   ['ARI', 'Arizona Cardinals'],
@@ -58,38 +49,38 @@ TEAMS.each do |team|
   Team.create short_name: team.first, name: team.second, sport: Sport.first
 end
 
-Week.create tournament: Tournament.first, number: 1
-Week.create tournament: Tournament.first, number: 2
-Week.create tournament: Tournament.first, number: 3
-Week.create tournament: Tournament.first, number: 4
-Week.create tournament: Tournament.first, number: 5
-Week.create tournament: Tournament.first, number: 6
-Week.create tournament: Tournament.first, number: 7
-Week.create tournament: Tournament.first, number: 8
-Week.create tournament: Tournament.first, number: 9
-Week.create tournament: Tournament.first, number: 10
-Week.create tournament: Tournament.first, number: 11
-Week.create tournament: Tournament.first, number: 12
-Week.create tournament: Tournament.first, number: 13
-Week.create tournament: Tournament.first, number: 14
-Week.create tournament: Tournament.first, number: 15
-Week.create tournament: Tournament.first, number: 16
-Week.create tournament: Tournament.first, number: 17
+# Week.create tournament: Tournament.first, number: 1
+# Week.create tournament: Tournament.first, number: 2
+# Week.create tournament: Tournament.first, number: 3
+# Week.create tournament: Tournament.first, number: 4
+# Week.create tournament: Tournament.first, number: 5
+# Week.create tournament: Tournament.first, number: 6
+# Week.create tournament: Tournament.first, number: 7
+# Week.create tournament: Tournament.first, number: 8
+# Week.create tournament: Tournament.first, number: 9
+# Week.create tournament: Tournament.first, number: 10
+# Week.create tournament: Tournament.first, number: 11
+# Week.create tournament: Tournament.first, number: 12
+# Week.create tournament: Tournament.first, number: 13
+# Week.create tournament: Tournament.first, number: 14
+# Week.create tournament: Tournament.first, number: 15
+# Week.create tournament: Tournament.first, number: 16
+# Week.create tournament: Tournament.first, number: 17
 
-# Create all weeks
-Tournament.first.weeks.each do |week|
-  doc = Nokogiri::HTML(open("https://www.espn.com/nfl/schedule/_/week/#{week.number}"))
-  dates = doc.xpath('//td/@data-date').map { |d| d.text }
-  co = 0
-  doc.css('div .responsive-table-wrap').each do |table|
-    table.css('tbody').each do |body|
-      body.css('tr').each do |match|
-        Match.create visit_team: Team.find_by(short_name: match.css('td .team-name abbr').first.text),
-                     home_team: Team.find_by(short_name: match.css('td .team-name abbr').last.text),
-                     start_time: dates[co],
-                     week: week
-        co = co + 1
-      end
-    end
-  end
-end
+# Create all week matches
+# Tournament.first.weeks.each do |week|
+#   doc = Nokogiri::HTML(open("https://www.espn.com/nfl/schedule/_/week/#{week.number}/seasontype/2"))
+#   dates = doc.xpath('//td/@data-date').map { |d| d.text }
+#   co = 0
+#   doc.css('div .responsive-table-wrap').each do |table|
+#     table.css('tbody').each do |body|
+#       body.css('tr').each do |match|
+#         Match.create visit_team: Team.find_by(short_name: match.css('td .team-name abbr').first.text),
+#                      home_team: Team.find_by(short_name: match.css('td .team-name abbr').last.text),
+#                      start_time: dates[co],
+#                      week: week
+#         co = co + 1
+#       end
+#     end
+#   end
+# end
