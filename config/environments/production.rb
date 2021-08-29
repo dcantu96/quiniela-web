14df6ed51f1bbe30da10971cb894695062e5ed37
David Cantu <davidcantu966@gmail.com>
2021-08-29 17:58:41 -0500

    added gmail account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,20 +63,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "quiniela_web_production"
 
   #setup mail for go daddy
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'qnflmty.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_options = { from: ENV['EMAIL'] }
+  config.action_mailer.default_url_options = { host: 'qnflmty.com' }
+
+  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    address:              'smtpout.secureserver.net',
-    port:                 25,
-    domain:               'quiniela-web.herokuapp.com',
-    user_name:            ENV['EMAIL'],
-    password:             ENV['PASSWORD'],
-    authentication:       :login,
-    enable_starttls_auto: false
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['EMAIL'],
+    :password             => ENV['PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
