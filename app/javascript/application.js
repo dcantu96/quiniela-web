d6f0b3cbaf2a363128a9476e8b8241f3985f9473
David Cantu Martinez <davidcantu966@gmail.com>
2022-08-15 00:18:08 -0700

    fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,8 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@fortawesome/fontawesome-free"
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import toaster from "sweetalert2"
+
+export { toaster }
