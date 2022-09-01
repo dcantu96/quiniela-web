766c3db5153c64297bc2aeb619f9db7de82b4353
David Cantu <davidcantu966@gmail.com>
2022-09-01 10:51:29 -0500

    add sidekiq config

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+redis_url = Rails.env.production? ? ENV['REDIS_URL'] : 'redis://127.0.0.1:6379'
+
+Sidekiq.configure_server do |config|   
+  config.redis = { url: redis_url } 
+end
+
+Sidekiq.configure_client do |config|   
+  config.redis = { url: redis_url } 
+end
