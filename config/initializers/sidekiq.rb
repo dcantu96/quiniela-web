a2fa9f74f1162579acea9b413f5f5136504d801e
David Cantu <davidcantu966@gmail.com>
2022-09-01 11:04:00 -0500

    fix: redis network timeout

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,7 @@
-redis_url = Rails.env.production? ? ENV['REDIS_URL'] : 'redis://127.0.0.1:6379'
-
 Sidekiq.configure_server do |config|   
-  config.redis = { url: redis_url } 
+  config.redis = { url: Rails.env.production? ? ENV['REDIS_URL'] : 'redis://127.0.0.1:7372/0', network_timeout: 5 } 
 end
 
 Sidekiq.configure_client do |config|   
-  config.redis = { url: redis_url } 
+  config.redis = { url: Rails.env.production? ? ENV['REDIS_URL'] : 'redis://127.0.0.1:7372/0', network_timeout: 5 } 
 end
