7db1ab92e520bb44f654212dfbce02e0095ff76e
David Cantu <davidcantu966@gmail.com>
2022-09-07 15:56:01 -0500

    fix: add request validation

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,4 +3,12 @@ class Request < ApplicationRecord
   belongs_to :account
   scope :pending, -> { where denied: false }
   scope :denied, -> { where denied: true }
+  validates_uniqueness_of :group, scope: [:account]
+  validate :membership_not_present
+
+  def membership_not_present
+    if account.memberships.where group: group
+      errors.add(:request, "already exists")
+    end
+  end
 end
