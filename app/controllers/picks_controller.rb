534d31c398efad88e277a9441d6a353cbb739a96
David Cantu <davidcantu966@gmail.com>
2022-09-25 12:12:20 -0500

    fix: update update meesage

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -4,14 +4,11 @@ class PicksController < ApplicationController
   def update
     respond_to do |format|
       if @pick.updatable? && @pick.update(pick_params)
-        if @pick.match.untie
+        if pick_params[:points].present?
           format.html { redirect_to picks_membership_path(@pick.membership_week.membership), notice: "Monday night score successfully set to #{@pick.points}" }
         else
           format.html { redirect_to picks_membership_path(@pick.membership_week.membership), notice: "Pick successfully set to #{@pick.picked_team.short_name}" }
         end
-        format.js
-      else
-        format.js { render 'error' }
       end
     end
   end
