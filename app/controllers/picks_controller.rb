c0f385280b5f19518bd59b0da3560c84cf18e105
David Cantu <davidcantu966@gmail.com>
2022-09-25 13:06:57 -0500

    fix: add turbo reload

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -5,8 +5,10 @@ class PicksController < ApplicationController
     respond_to do |format|
       if @pick.updatable? && @pick.update(pick_params)
         if pick_params[:points].present?
+          format.turbo_stream { flash.now[:notice] = "Monday night score successfully set to #{@pick.points}" }
           format.html { redirect_to picks_membership_path(@pick.membership_week.membership), notice: "Monday night score successfully set to #{@pick.points}" }
         else
+          format.turbo_stream { flash.now[:notice] = "Pick successfully set to #{@pick.picked_team.short_name}" }
           format.html { redirect_to picks_membership_path(@pick.membership_week.membership), notice: "Pick successfully set to #{@pick.picked_team.short_name}" }
         end
       end
