49588a7f271df95d4ac8d9c01b53384b60e93821
David Cantu <davidcantu966@gmail.com>
2024-09-03 22:22:42 -0600

    fix: replace params with props

--- app/jobs/accept_request_job.rb
+++ app/jobs/accept_request_job.rb
@@ -2,7 +2,7 @@ class AcceptRequestJob < ApplicationJob
   queue_as :default
 
   def perform(request_id)
-    request = Request.find(params[:id])
+    request = Request.find(request_id)
     membership = Membership.new group: request.group, account: request.account
     if membership.valid? && request.destroy && membership.save
       RequestMailer.request_accepted(membership).deliver_later
--- app/jobs/reject_request_job.rb
+++ app/jobs/reject_request_job.rb
@@ -2,7 +2,7 @@ class RejectRequestJob < ApplicationJob
   queue_as :default
 
   def perform(request_id)
-    request = Request.find(params[:id])
+    request = Request.find(request_id)
     if request.update denied: true
       RequestMailer.request_denied(request).deliver_later
     end
